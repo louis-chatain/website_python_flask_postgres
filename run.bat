9a679777c5a28231451eb46c774801146f79402e
Louis Chatain <louischatain123@gmail.com>
2025-09-22 20:38:36 +0200

    can now run the app with "python backend/app.py"

--- run.bat
+++
@@ -1,4 +0,0 @@
-set FLASK_APP=app
-set FLASK_ENV=development
-set FLASK_RUN_PORT=8000
-flask run
--- run.sh
+++
@@ -1,8 +0,0 @@
-export FLASK_APP=backend/app
-export FLASK_ENV=development
-export FLASK_RUN_PORT=8000
-export FLASK_DEBUG=1
-
-flask run
-
-# or type "python backend/app.py in the terminal
